4565a30cb07f4682c867150913d323901dea3130
Vaishnavi K <vaishnavikashyap936@gmail.com>
2025-10-19 12:57:02 +0530

    Refactor password generation logic for clarity

--- Python/Password Generator/password_generator.py
+++ Python/Password Generator/password_generator.py
@@ -6,10 +6,10 @@ lower_alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['*','/','.','?','[]',';','@','!','$','#','^','%','&','+','-']
 
-positive_response = ['y','yes','ok','Y']
-negative_response = ['n','N','no']
+positive_response = ['y','yes','ok']
+negative_response = ['n','no']
 
-sym_include = input("Include Symbols? (y/n) ")
+sym_include = input("Include Symbols? (y/n) ").lower()
 
 if sym_include in positive_response:	
 	characters = upper_alphabet + lower_alphabet + numbers + symbols
@@ -19,14 +19,8 @@ elif sym_include in negative_response:
 print('\n')
 
 length = int(input('Length of password: '))
-
-n = 0
 password = ""
-index = len(characters)
-
-while length != n:
-	password = password + characters[random.randint(0,index - 1)]		
-	n = n + 1
-
+for i in range(length):
+	password=password + random.choice(characters)
 print('\n')
 print(password)
